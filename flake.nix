e17dbab524e65ceb0b5c03412c4a6c2cbaca8d45
hugosenari <hugosenari@gmail.com>
2022-03-12 04:02:09 -0300

    chore: re index ttmath

---
+++ flake.nix
@@ -0,0 +1,23 @@
+{
+  description = ''A Nim wrapper for ttmath: big numbers with fixed size'';
+
+  inputs.flakeNimbleLib.owner = "riinr";
+  inputs.flakeNimbleLib.ref   = "master";
+  inputs.flakeNimbleLib.repo  = "nim-flakes-lib";
+  inputs.flakeNimbleLib.type  = "github";
+  inputs.flakeNimbleLib.inputs.nixpkgs.follows = "nixpkgs";
+  
+  inputs."ttmath-master".url = "path:./master";
+  inputs."ttmath-master".inputs.nixpkgs.follows = "nixpkgs";
+  inputs."ttmath-master".inputs.flakeNimbleLib.follows = "flakeNimbleLib";
+  
+  outputs = { self, nixpkgs, flakeNimbleLib, ...}@inputs:
+  let 
+    lib  = flakeNimbleLib.lib;
+    args = ["self" "nixpkgs" "flakeNimbleLib"];
+  in lib.mkProjectOutput {
+    inherit self nixpkgs;
+    meta = builtins.fromJSON (builtins.readFile ./meta.json);
+    refs = builtins.removeAttrs inputs args;
+  };
+}
---
+++ master/flake.nix
@@ -0,0 +1,26 @@
+{
+  description = ''A Nim wrapper for ttmath: big numbers with fixed size'';
+
+  inputs.flakeNimbleLib.owner = "riinr";
+  inputs.flakeNimbleLib.ref   = "master";
+  inputs.flakeNimbleLib.repo  = "nim-flakes-lib";
+  inputs.flakeNimbleLib.type  = "github";
+  inputs.flakeNimbleLib.inputs.nixpkgs.follows = "nixpkgs";
+  
+  inputs.src-ttmath-master.flake = false;
+  inputs.src-ttmath-master.owner = "status-im";
+  inputs.src-ttmath-master.ref   = "refs/heads/master";
+  inputs.src-ttmath-master.repo  = "nim-ttmath";
+  inputs.src-ttmath-master.type  = "github";
+  
+  outputs = { self, nixpkgs, flakeNimbleLib, ...}@deps:
+  let 
+    lib  = flakeNimbleLib.lib;
+    args = ["self" "nixpkgs" "flakeNimbleLib" "src-ttmath-master"];
+  in lib.mkRefOutput {
+    inherit self nixpkgs ;
+    src  = deps."src-ttmath-master";
+    deps = builtins.removeAttrs deps args;
+    meta = builtins.fromJSON (builtins.readFile ./meta.json);
+  };
+}
